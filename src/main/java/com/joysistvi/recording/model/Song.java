80605bab93bb66b0a7eaba1f2ca85d6ace42250f
djkier <djjustinekier@gmail.com>
2026-09-25 01:00:32 +0800

    Add Song model class

--- src/main/java/com/joysistvi/recording/model/Song.java
+++ src/main/java/com/joysistvi/recording/model/Song.java
@@ -1,4 +1,57 @@
 package com.joysistvi.recording.model;
 
 public class Song {
+    private int id;
+    private String title;
+    private int length;
+    private String genre;
+    private int albumId;
+
+    public Song(int id, String title, int length, String genre, int albumId) {
+        this.id = id;
+        this.title = title;
+        this.length = length;
+        this.genre = genre;
+        this.albumId = albumId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(int albumId) {
+        this.albumId = albumId;
+    }
 }
