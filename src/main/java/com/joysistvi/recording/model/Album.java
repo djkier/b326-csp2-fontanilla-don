8b954e02f580d7e05c067ad185569b75f83c1424
djkier <djjustinekier@gmail.com>
2026-09-25 00:59:22 +0800

    Add Playlist model class

--- src/main/java/com/joysistvi/recording/model/Album.java
+++ src/main/java/com/joysistvi/recording/model/Album.java
@@ -1,4 +1,48 @@
 package com.joysistvi.recording.model;
 
+
 public class Album {
+    private int id;
+    private String name;
+    private int year;
+    private int artistId;
+
+    public Album(int id, String name, int year, int artistId ) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.artistId = artistId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(int artistId) {
+        this.artistId = artistId;
+    }
 }
--- src/main/java/com/joysistvi/recording/model/Playlist.java
+++ src/main/java/com/joysistvi/recording/model/Playlist.java
@@ -1,4 +1,39 @@
 package com.joysistvi.recording.model;
 
+import java.time.LocalDateTime;
+
 public class Playlist {
+    private int id;
+    private LocalDateTime dateCreated;
+    private int userId;
+
+    public Playlist(int id, LocalDateTime dateCreated, int userId) {
+        this.id = id;
+        this.dateCreated = dateCreated;
+        this.userId = userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
